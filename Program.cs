3427610760eb691a692ca0c8145b5daa8f83e201
Gasan Nuriev <C:\Users\Gasan\AppData\Roaming\The Bat!>
2023-02-15 13:12:44 +0300

    new commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число  ");      // вводим начальную информацию 
+string inputValue = Console.ReadLine();     // для первых чисел присваиваем переменных из консоли
+
+ int Value = Convert.ToInt32(inputValue);
+ 
+ 
+ if (Value % 2 == 0)  
+{
+    System.Console.WriteLine($"Число {Value} четное число ");
+}
+else
+{
+    System.Console.WriteLine ($"Число {Value} не четные число");
+}
+  
